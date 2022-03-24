83879eb10bdf11fd520b2ac72b6e7c96444b9b35
MIGUEL <MIGUEL@192.168.1.15>
2022-03-23 20:01:14 -0500

    ...

--- src/POA/Controlador/con_periodoAcademico.java
+++ src/POA/Controlador/con_periodoAcademico.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package POA.Controlador;
+import POA.Modelo.PeriodoacademicoBD;
 import POA.Vista.*;
 
 /**
@@ -11,11 +12,16 @@ import POA.Vista.*;
  */
 public class con_periodoAcademico {
     private final vis_PeriodoAcademico vista;
+    PeriodoacademicoBD periodo;
 
     public con_periodoAcademico(vis_PeriodoAcademico vista) {
         this.vista = vista;
         vista.setVisible(true);
     }
     
+    public void ingresar(){
+        periodo.setFechainicio(vista.getDateFechaInicio().getDate());
+        periodo.setFechafin(vista.getDateFechaFin().getDate());
+    }
     
 }

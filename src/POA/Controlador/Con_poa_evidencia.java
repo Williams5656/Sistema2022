921494c71d57cb507c187134f423e10dfea76071
ASUS <ASUS@LAPTOP-ABBHQ4GU.mshome.net>
2022-03-28 20:12:02 -0500

    vispoa

--- src/POA/Controlador/Con_poa_evidencia.java
+++ src/POA/Controlador/Con_poa_evidencia.java
@@ -5,7 +5,14 @@
  */
 package POA.Controlador;
 
+import POA.Modelo.CarreraMD;
+import POA.Modelo.ProyectoMD;
+import POA.Modelo.ProyectoBD;
+import POA.Modelo.CarreraBD;
+import POA.Modelo.PoaBD;
 import POA.Vista.vis_poa_evidencia;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,12 +20,34 @@ import POA.Vista.vis_poa_evidencia;
  */
 public class Con_poa_evidencia {
       private final vis_poa_evidencia vista;
+      private ArrayList <ProyectoMD> listaProyectos = new ArrayList<>();
+      private List<POA.Modelo.CarreraMD> listaCarreras = new ArrayList<>();
+      private List<POA.Modelo.PoaMD> listaPoa = new ArrayList<>();
+      private ProyectoBD baseDatosProyecto = new ProyectoBD();
+      private CarreraBD baseDatosCarrera = new CarreraBD();
+      private PoaBD baseDatosPoa = new PoaBD();
     
-
     public Con_poa_evidencia(vis_poa_evidencia vista) {
         this.vista = vista;
         vista.setVisible(true);
-       
-
+    }
+    public void cargarComboProyecto(){
+        vista.getCbx_proyecto().removeAllItems();
+        listaProyectos = baseDatosProyecto.mostrarDatos();       
+        for (ProyectoMD proyectos : listaProyectos){
+            if (Con_Poa1.getId_poa() == proyectos.getId_Poa()){
+                vista.getCbx_proyecto().addItem("Proyecto: " + proyectos.getNum_proyecto_carrera()); 
+            }
+        }
+    }
+    public void cargarComboCarrera(){
+        listaCarreras = baseDatosCarrera.mostrardatos();
+        listaPoa = baseDatosPoa.mostrarDatos();
+        vista.getCbx_carrera().removeAllItems();
+        for (int i = 0; i < listaPoa.size(); i++) {
+            if(String.valueOf(listaPoa.get(i).getId_carrera()).equals(String.valueOf(listaCarreras.get(i).getCodigo_carrera()))){
+                vista.getCbx_proyecto().addItem(listaCarreras.get(i).getNombre_carrera());
+            }
+        }
     }
 }

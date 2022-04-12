d167112a828e1496b979c995dc6b6db38bee88e0
KarenYanza <97999826+KarenYanza@users.noreply.github.com>
2022-04-12 17:37:46 -0500

    AreaCarreraModificacion

--- src/POA/Controlador/Con_AreaCarrera.java
+++ src/POA/Controlador/Con_AreaCarrera.java
@@ -277,7 +277,6 @@ public class Con_AreaCarrera {
         com1 = basePerfil.vResponsable(id, codigoCarrera, cedula);
         int a = 1;
         int b = 1;
-        int c=0;
         if (comparacion1 != 0) {
             if (com == 0) {
                 a = 0;
@@ -294,11 +293,8 @@ public class Con_AreaCarrera {
                 JOptionPane.showMessageDialog(null, "El perfil ya existe");
                 vista.getComboPerfil().setSelectedIndex(0);
             }
-        }else if (comparacion != 0) {
-            JOptionPane.showMessageDialog(null, "El perfil ya existe");
-            vista.getComboPerfil().setSelectedIndex(0);
         }
-        if (a == 0 || b == 0) {
+        if (a == 0 && b == 0) {
             int resp2 = JOptionPane.showConfirmDialog(null, "CONFIRME SI ESTA SEGURO DE MODIFICAR");
             if (resp2 == 0) {
                 if (bdarea.modificar(id)) {
